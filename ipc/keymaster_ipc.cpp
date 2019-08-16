a30d78cd7388d14788bfdc9404a3b612065c8651
Ji Luo <ji.luo@nxp.com>
2019-08-16 12:19:26 +0800

    MA-15321-1 Support secure unlock
    Add ipc command to verify the secure unlock credential.
    The unlock credential is generated from serial number
    and be encrypted with mppubk.
    
    Test: secure unlock credential verify.
    
    Change-Id: I9eee8a71ea7d1b8c99c2ca7118375aa684ae7fc4
    Signed-off-by: Ji Luo <ji.luo@nxp.com>

--- ipc/keymaster_ipc.cpp
+++ ipc/keymaster_ipc.cpp
@@ -365,7 +365,8 @@ static bool cmd_is_from_bootloader(uint32_t cmd) {
             cmd == KM_ATAP_SET_CA_RESPONSE_BEGIN ||
             cmd == KM_ATAP_SET_CA_RESPONSE_UPDATE ||
             cmd == KM_ATAP_SET_CA_RESPONSE_FINISH || cmd == KM_ATAP_READ_UUID ||
-            cmd == KM_SET_PRODUCT_ID || cmd == KM_GET_MPPUBK);
+            cmd == KM_SET_PRODUCT_ID || cmd == KM_GET_MPPUBK ||
+            cmd == KM_VERIFY_SECURE_UNLOCK);
 }
 
 // Returns true if |cmd| can be used before the configure command
@@ -438,6 +439,11 @@ static long keymaster_dispatch_non_secure(keymaster_chan_ctx* ctx,
         LOG_D("Dispatching GET_MPPUBK, size %d", payload_size);
         return do_dispatch(&TrustyKeymaster::GetMppubk, msg, payload_size, out,
                            out_size);
+
+    case KM_VERIFY_SECURE_UNLOCK:
+        LOG_D("Dispatching KM_VERIFY_UNLOCK, size %d", payload_size);
+        return do_dispatch(&TrustyKeymaster::VerifySecureUnlock, msg,
+                           payload_size, out, out_size);
 
     case KM_GET_VERSION:
         LOG_D("Dispatching GET_VERSION, size: %d", payload_size);
